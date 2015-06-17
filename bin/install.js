4db08f3a82c9837a8915c1dfa784a1a1a229b6a1
Carlos Villuendas <carlosvillu@gmail.com>
2015-06-17 10:51:04 +0200

    Add babel stage 0

--- bin/install.js
+++ bin/install.js
@@ -5,5 +5,6 @@ var Validate = require('git-validate');
 Validate.copy('../eslintrc', '.eslintrc');
 Validate.copy('../editorconfig', '.editorconfig');
 Validate.copy('../jscsrc', '.jscsrc');
+Validate.copy('../babelrc', '.babelrc');
 
 Validate.installHooks('pre-commit');
