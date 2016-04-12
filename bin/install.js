2898642527ad7c70620932268485eca24788ef79
davecarter <dave74@gmail.com>
2016-04-12 12:18:07 +0200

    renamed files for consistency

--- bin/install.js
+++ bin/install.js
@@ -3,8 +3,8 @@
 var Validate = require('git-validate');
 
 Validate.copy('../.eslintignore', '.eslintignore', { overwrite: true });
-Validate.copy('../eslintrc', '.eslintrc.json', { overwrite: true });
-Validate.copy('../editorconfig', '.editorconfig', { overwrite: true });
+Validate.copy('../eslintrc.json', 'eslintrc.json', { overwrite: true });
+Validate.copy('../.editorconfig', '.editorconfig', { overwrite: true });
 Validate.copy('../scss-lint.yml', '.scss-lint.yml', { overwrite: true });
 Validate.copy('../sass-lint.yml', '.sass-lint.yml', { overwrite: true });
 
