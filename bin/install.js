31f2bba339a05f1b5c9c87939627e8a9d6970f5d
Miguel Angel Duran Garcia <miguelangel.duran@scmspain.com>
2016-07-22 12:05:37 +0200

    First commit for moving to standard JS

--- bin/install.js
+++ bin/install.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
-var Validate = require('git-validate');
+'use strict'
 
-Validate.copy('../.eslintignore', '.eslintignore', { overwrite: true });
-Validate.copy('../eslintrc', '.eslintrc', { overwrite: true });
-Validate.copy('../editorconfig', '.editorconfig', { overwrite: true });
-Validate.copy('../scss-lint.yml', '.scss-lint.yml', { overwrite: true });
-Validate.copy('../sass-lint.yml', '.sass-lint.yml', { overwrite: true });
+var Validate = require('git-validate')
 
-Validate.installHooks('pre-commit');
+Validate.copy('../eslintignore', '.eslintignore', { overwrite: true })
+Validate.copy('../eslintrc', '.eslintrc', { overwrite: true })
+Validate.copy('../editorconfig', '.editorconfig', { overwrite: true })
+Validate.copy('../scss-lint.yml', '.scss-lint.yml', { overwrite: true })
+Validate.copy('../sass-lint.yml', '.sass-lint.yml', { overwrite: true })
+
+Validate.installHooks('pre-commit')
