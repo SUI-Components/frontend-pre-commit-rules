4584456e7084b7ecf1504701502c61799c9d4c4c
Miguel Angel Duran Garcia <miguelangel.duran@scmspain.com>
2016-08-31 17:52:35 +0200

    Add log and fix overwrite for scripts

--- bin/install.js
+++ bin/install.js
@@ -3,10 +3,11 @@
 'use strict'
 
 const OVERWRITE = 1
-
+const º = console.log
 const Validate = require('git-validate')
 
-console.log('Copying config files...')
+º('[start] frontend-pre-commit-rules')
+º('Copying config files...')
 
 Validate.copy('../config/.eslintrc', '.eslintrc', OVERWRITE)
 Validate.copy('../config/.eslintignore', '.eslintignore', OVERWRITE)
@@ -14,13 +15,14 @@ Validate.copy('../config/.editorconfig', '.editorconfig', OVERWRITE)
 Validate.copy('../config/.scss-lint.yml', '.scss-lint.yml', OVERWRITE)
 Validate.copy('../config/.sass-lint.yml', '.sass-lint.yml', OVERWRITE)
 
-console.log('Files copied! Adding scripts to package.json')
+º('Files copied! Adding scripts to package.json')
 
-Validate.installScript('lint:js', 'lint-js --ext .js, .jsx src/', OVERWRITE)
-Validate.installScript('lint:sass', 'lint-sass src/**/*.scss -c -v', OVERWRITE)
-Validate.installScript('lint', 'npm run lint:js && npm run lint:sass', OVERWRITE)
+Validate.installScript('lint:js', 'lint-js --ext .js, .jsx src/', {overwrite: OVERWRITE})
+Validate.installScript('lint:sass', 'lint-sass src/**/*.scss -c -v', {overwrite: OVERWRITE})
+Validate.installScript('lint', 'npm run lint:js && npm run lint:sass', {overwrite: OVERWRITE})
 
-console.log('Scripts added! Add hooks and configure them')
+º('Scripts added! Add hooks and configure them')
 Validate.installHooks('pre-commit')
 Validate.configureHook('pre-commit', ['lint', 'test'])
-console.log('Pre-commit-rules completely configured')
+º('Pre-commit-rules completely configured')
+º('[finished] frontend-pre-commit-rules')
