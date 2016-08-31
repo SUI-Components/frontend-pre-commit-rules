8c23983976c9bef0aca487107939218a19890c31
Miguel Angel Duran Garcia <miguelangel.duran@scmspain.com>
2016-08-31 17:34:03 +0200

    Add some log info when installing files and hooks

--- bin/install.js
+++ bin/install.js
@@ -14,9 +14,13 @@ Validate.copy('../config/.editorconfig', '.editorconfig', OVERWRITE)
 Validate.copy('../config/.scss-lint.yml', '.scss-lint.yml', OVERWRITE)
 Validate.copy('../config/.sass-lint.yml', '.sass-lint.yml', OVERWRITE)
 
+console.log('Files copied! Adding scripts to package.json')
+
 Validate.installScript('lint:js', 'lint-js --ext .js, .jsx src/', OVERWRITE)
 Validate.installScript('lint:sass', 'lint-sass src/**/*.scss -c -v', OVERWRITE)
 Validate.installScript('lint', 'npm run lint:js && npm run lint:sass', OVERWRITE)
 
+console.log('Scripts added! Add hooks and configure them')
 Validate.installHooks('pre-commit')
 Validate.configureHook('pre-commit', ['lint', 'test'])
+console.log('Pre-commit-rules completely configured')
