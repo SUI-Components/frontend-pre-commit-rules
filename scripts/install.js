2c803f9bfdc521f81ef84ebadb4026c3bcf0e507
Miguel Angel Duran Garcia <miguelangel.duran@scmspain.com>
2016-09-07 15:34:57 +0200

    Fix options to overwrite files

--- scripts/install.js
+++ scripts/install.js
@@ -2,24 +2,24 @@
 
 'use strict'
 
-const OVERWRITE = true
+const OPTIONS = { overwrite: 1 }
 const º = console.log
 const Validate = require('git-validate')
 
 º('[start] frontend-pre-commit-rules')
 º('Copying config files...')
 
-Validate.copy('../config/.eslintrc', '.eslintrc', OVERWRITE)
-Validate.copy('../config/.eslintignore', '.eslintignore', OVERWRITE)
-Validate.copy('../config/.editorconfig', '.editorconfig', OVERWRITE)
-Validate.copy('../config/.scss-lint.yml', '.scss-lint.yml', OVERWRITE)
-Validate.copy('../config/.sass-lint.yml', '.sass-lint.yml', OVERWRITE)
+Validate.copy('../config/.eslintrc', '.eslintrc', OPTIONS)
+Validate.copy('../config/.eslintignore', '.eslintignore', OPTIONS)
+Validate.copy('../config/.editorconfig', '.editorconfig', OPTIONS)
+Validate.copy('../config/.scss-lint.yml', '.scss-lint.yml', OPTIONS)
+Validate.copy('../config/.sass-lint.yml', '.sass-lint.yml', OPTIONS)
 
 º('Files copied! Adding scripts to package.json')
 
-Validate.installScript('lint:js', 'node_modules/.bin/eslint --ext .js,.jsx src/', {overwrite: OVERWRITE})
-Validate.installScript('lint:sass', 'node_modules/.bin/sass-lint src/**/*.scss -c -v', {overwrite: OVERWRITE})
-Validate.installScript('lint', 'npm run lint:js && npm run lint:sass', {overwrite: OVERWRITE})
+Validate.installScript('lint:js', 'node_modules/.bin/eslint --ext .js,.jsx src/', OPTIONS)
+Validate.installScript('lint:sass', 'node_modules/.bin/sass-lint src/**/*.scss -c -v', OPTIONS)
+Validate.installScript('lint', 'npm run lint:js && npm run lint:sass', OPTIONS)
 
 º('Scripts added! Add hooks and configure them')
 Validate.installHooks('pre-commit')
