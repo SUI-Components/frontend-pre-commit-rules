0ee505a36bf8ad7c3bd12d7d79ab01259301c366
Miguel Angel Duran Garcia <miguelangel.duran@scmspain.com>
2016-10-20 16:49:16 +0200

    Restore

--- scripts/install.js
+++ scripts/install.js
@@ -3,15 +3,11 @@
 'use strict'
 
 const OPTIONS = { overwrite: 1 }
-const log = console.log
+const º = console.log
 const Validate = require('git-validate')
-const path = require('path')
 
-const packagePath = path.resolve(process.env.PWD, './package.json')
-const packageJSON = require(packagePath)
-
-log('[start] frontend-pre-commit-rules')
-log('Copying config files...')
+º('[start] frontend-pre-commit-rules')
+º('Copying config files...')
 
 Validate.copy('../config/.eslintrc', '.eslintrc', OPTIONS)
 Validate.copy('../config/.eslintignore', '.eslintignore', OPTIONS)
@@ -19,20 +15,14 @@ Validate.copy('../config/.editorconfig', '.editorconfig', OPTIONS)
 Validate.copy('../config/.scss-lint.yml', '.scss-lint.yml', OPTIONS)
 Validate.copy('../config/.sass-lint.yml', '.sass-lint.yml', OPTIONS)
 
-log('Files copied! Adding scripts to package.json')
+º('Files copied! Adding scripts to package.json')
 
-log('Package where installing pre-commit-rules: ', packageJSON.name)
-
-const isSelfPackage = packageJSON.name === '@schibstedspain/frontend-pre-commit-rules'
-const linterJs = isSelfPackage ? 'eslint' : 'linter-js'
-const linterSass = isSelfPackage ? 'sass-lint' : 'linter-sass'
-
-Validate.installScript('lint:js', `node_modules/.bin/${linterJs} --ext .js,.jsx src/`, OPTIONS)
-Validate.installScript('lint:sass', `node_modules/.bin/${linterSass} src/**/*.scss -c -v`, OPTIONS)
+Validate.installScript('lint:js', 'node_modules/.bin/eslint --ext .js,.jsx src/', OPTIONS)
+Validate.installScript('lint:sass', 'node_modules/.bin/sass-lint src/**/*.scss -c -v', OPTIONS)
 Validate.installScript('lint', 'npm run lint:js && npm run lint:sass', OPTIONS)
 
-log('Scripts added! Add hooks and configure them')
+º('Scripts added! Add hooks and configure them')
 Validate.installHooks('pre-commit')
 Validate.configureHook('pre-commit', ['lint', 'test'])
-log('Pre-commit-rules completely configured')
-log('[finished] frontend-pre-commit-rules\n')
+º('Pre-commit-rules completely configured')
+º('[finished] frontend-pre-commit-rules\n')
