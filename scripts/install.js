25356886cd75917985a23db54f49ddd0449ef3e9
Miguel Angel Duran Garcia <miguelangel.duran@scmspain.com>
2016-10-20 13:58:13 +0200

    Make it compatible with self pre-commit-rules and binaries

--- scripts/install.js
+++ scripts/install.js
@@ -3,11 +3,15 @@
 'use strict'
 
 const OPTIONS = { overwrite: 1 }
-const º = console.log
+const log = console.log
 const Validate = require('git-validate')
+const path = require('path')
 
-º('[start] frontend-pre-commit-rules')
-º('Copying config files...')
+const packagePath = path.resolve(process.env.PWD, './package.json')
+const packageJSON = require(packagePath)
+
+log('[start] frontend-pre-commit-rules')
+log('Copying config files...')
 
 Validate.copy('../config/.eslintrc', '.eslintrc', OPTIONS)
 Validate.copy('../config/.eslintignore', '.eslintignore', OPTIONS)
@@ -15,14 +19,20 @@ Validate.copy('../config/.editorconfig', '.editorconfig', OPTIONS)
 Validate.copy('../config/.scss-lint.yml', '.scss-lint.yml', OPTIONS)
 Validate.copy('../config/.sass-lint.yml', '.sass-lint.yml', OPTIONS)
 
-º('Files copied! Adding scripts to package.json')
+log('Files copied! Adding scripts to package.json')
 
-Validate.installScript('lint:js', 'node_modules/.bin/linter-js --ext .js,.jsx src/', OPTIONS)
-Validate.installScript('lint:sass', 'node_modules/.bin/linter-sass src/**/*.scss -c -v', OPTIONS)
+log('Package where installing pre-commit-rules: ', packageJSON.name)
+
+const isSelfPackage = packageJSON.name === '@schibstedspain/frontend-pre-commit-rules'
+const linterJs = isSelfPackage ? 'eslint' : 'linter-js'
+const linterSass = isSelfPackage ? 'sass-lint' : 'linter-sass'
+
+Validate.installScript('lint:js', `node_modules/.bin/${linterJs} --ext .js,.jsx src/`, OPTIONS)
+Validate.installScript('lint:sass', `node_modules/.bin/${linterSass} src/**/*.scss -c -v`, OPTIONS)
 Validate.installScript('lint', 'npm run lint:js && npm run lint:sass', OPTIONS)
 
-º('Scripts added! Add hooks and configure them')
+log('Scripts added! Add hooks and configure them')
 Validate.installHooks('pre-commit')
 Validate.configureHook('pre-commit', ['lint', 'test'])
-º('Pre-commit-rules completely configured')
-º('[finished] frontend-pre-commit-rules\n')
+log('Pre-commit-rules completely configured')
+log('[finished] frontend-pre-commit-rules\n')
