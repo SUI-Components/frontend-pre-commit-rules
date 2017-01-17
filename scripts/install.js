a2d259e6e2b9bdab122bd7f96eb74f3681bd2f37
Miguel Ángel Durán <miguel@miduga.es>
2017-01-17 13:47:08 +0100

    Add test directory and change log name

--- scripts/install.js
+++ scripts/install.js
@@ -3,11 +3,11 @@
 'use strict'
 
 const OPTIONS = { overwrite: 1 }
-const º = console.log
+const log = console.log
 const Validate = require('git-validate')
 
-º('[start] frontend-pre-commit-rules')
-º('Copying config files...')
+log('[start] frontend-pre-commit-rules')
+log('Copying config files...')
 
 Validate.copy('../config/.eslintrc', '.eslintrc', OPTIONS)
 Validate.copy('../config/.eslintignore', '.eslintignore', OPTIONS)
@@ -15,14 +15,21 @@ Validate.copy('../config/.editorconfig', '.editorconfig', OPTIONS)
 Validate.copy('../config/.scss-lint.yml', '.scss-lint.yml', OPTIONS)
 Validate.copy('../config/.sass-lint.yml', '.sass-lint.yml', OPTIONS)
 
-º('Files copied! Adding scripts to package.json')
+log('Files copied! Adding scripts to package.json')
 
-Validate.installScript('lint:js', 'node_modules/.bin/eslint --ext .js,.jsx src/', OPTIONS)
+Validate.installScript('lint:js', 'node_modules/.bin/eslint --ext .js,.jsx src/ test/', OPTIONS)
 Validate.installScript('lint:sass', 'node_modules/.bin/sass-lint src/**/*.scss -c -v', OPTIONS)
 Validate.installScript('lint', 'npm run lint:js && npm run lint:sass', OPTIONS)
 
-º('Scripts added! Add hooks and configure them')
+log('Scripts added! Add hooks and configure them')
 Validate.installHooks('pre-commit')
 Validate.configureHook('pre-commit', ['lint', 'test'])
-º('Pre-commit-rules completely configured')
-º('[finished] frontend-pre-commit-rules\n')
+
+// temporary fix until this is fixed: https://github.com/yarnpkg/yarn/issues/760
+// log('[temp] fix yarn binaries problem')
+// const fixYarnBinaries = require('./fix-yarn-binaries')
+// fixYarnBinaries()
+// log('[temp] ended fixing yarn binaries problem')
+
+log('Pre-commit-rules completely configured')
+log('[finished] frontend-pre-commit-rules\n')
